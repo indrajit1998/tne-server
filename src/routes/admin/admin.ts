fcb3680537446c8bddd64e32853d744888c56d7a
Ayush Chaturvedi <2200520100019@ietlucknow.ac.in>
2025-10-08 18:40:37 +0530

    admins

--- src/routes/admin/admin.ts
+++ src/routes/admin/admin.ts
@@ -2,6 +2,8 @@ import { application, Router } from "express";
 import { adminLogin } from "../../controllers/admin/adminauth";
 import { isAdminAuthMiddleware } from "../../middlewares/adminAuthMiddleware";
 import { consignmentStats, getEarningsStats, getUsersStats, requestStats, travelStats } from "../../controllers/admin/adminDashboard";
+import { deleteUser, manageUsers } from "../../controllers/admin/userManagement";
+import { getPrices, managePrices } from "../../controllers/admin/priceManagement";
 
 const adminRouts = Router();
 adminRouts.post("adminLogin", adminLogin)
@@ -12,5 +14,8 @@ adminRouts.get("/getConsignmentsStats", consignmentStats)
 adminRouts.get("/getEarningsStats", getEarningsStats)
 adminRouts.get("/getTravelStats", travelStats)
 adminRouts.get("/getRequestStats", requestStats)
-
+adminRouts.delete("/deleteUser", deleteUser)
+adminRouts.get("/manageUsers", manageUsers)
+adminRouts.get("/getPriceConfig", getPrices)
+adminRouts.patch("/managePrices", managePrices)
 export default adminRouts;
