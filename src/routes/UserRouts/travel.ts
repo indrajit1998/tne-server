ca52a91cc045973514988d6cdd29a726710d3828
Aditya Joshi <joshiaditya.aj07@gmail.com>
2025-10-15 17:45:26 +0530

    feat: added travelconsignment apis, get earning apis, strat, end and cancel travel apis and refactors

--- src/routes/UserRouts/travel.ts
+++ src/routes/UserRouts/travel.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 
 import { validate } from "../../../validator";
 import {
+  cancelTravel,
   createTravel,
+  endTravel,
   getTravels,
   locateTravel,
   locateTravelbyid,
+  startTravel,
 } from "../../controllers/UserController/travel";
 import isAuthMiddleware from "../../middlewares/authMiddleware";
 import { createTravelSchema } from "../../middlewares/travel.validator";
@@ -21,5 +24,8 @@ travelRouter.post(
 travelRouter.get("/getTravels", isAuthMiddleware, getTravels);
 travelRouter.get("/locateTravel", isAuthMiddleware, locateTravel);
 travelRouter.get("/locateTravelByid/:id", isAuthMiddleware, locateTravelbyid);
+travelRouter.post("/:travelId/start", isAuthMiddleware, startTravel);
+travelRouter.post("/:travelId/end", isAuthMiddleware, endTravel);
+travelRouter.post("/:travelId/cancel", isAuthMiddleware, cancelTravel);
 
 export default travelRouter;
